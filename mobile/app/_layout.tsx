ed505260e46606f878307188e56d503973218f22
SomethingMature64 <jojogana0@outlook.com>
2025-08-22 14:16:51 +0100

    Worked on the basics of the layout, understanding how layouts work in react native
    KEY COMMIT for study

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return(<SafeAreaView style={{flex:1}}> //To prevent our content from falling underneath the status bar
+    <Stack screenOptions={{headerShown:false}}/> //! Use this to hide the header bar
+  </SafeAreaView>
+  )
+  
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { StyleSheet,Text, TextInput, TouchableOpacity, View } from "react-native";
+import {Image} from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.contianer}>
+      <Text style = {styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
+      <Link href={"/about"}>About Screen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  contianer:{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+  },
+  text:{
+    color: "blue",
+    fontSize:40
+  },
+});
