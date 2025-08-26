fb98a4ceae34f90f8c0095ad868ee98d6c5b87a9
SomethingMature64 <jojogana0@outlook.com>
2025-08-26 12:19:06 +0100

    Stylesheets were made

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return(<SafeAreaView style={{flex:1}}> //To prevent our content from falling underneath the status bar
-    <Stack screenOptions={{headerShown:false}}/> //! Use this to hide the header bar
+  return(<SafeAreaView style={{flex:1}}> 
+    <Stack screenOptions={{headerShown:false}}/> 
   </SafeAreaView>
   )
   
